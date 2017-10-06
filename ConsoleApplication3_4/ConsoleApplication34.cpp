e2b7246afa4d01ef670f2bcd138746ec8e86431d
ponychan <rzsckbmx@163.com>
2017-10-06 11:45:55 +0800

    3.4.cpp
    求回文数

--- ConsoleApplication3_4/ConsoleApplication34.cpp
+++ ConsoleApplication3_4/ConsoleApplication34.cpp
@@ -1,11 +1,28 @@
 // ConsoleApplication34.cpp: 定义控制台应用程序的入口点。
-//
+//3.4.cpp寻找11-999之间自身，平方，；立方均为回文数的数字
 
 #include "stdafx.h"
+#include<iostream>
+using namespace std;
 
+bool choose(unsigned n) {
+	unsigned i = n;
+	unsigned m = 0;
+	while (i > 0) {
+		m = m * 10 + i % 10;
+		i /= 10;
+	}
+	return m == n;
+}
 
 int main()
 {
-    return 0;
+	for (unsigned m = 11; m<1000; m++)
+		if (choose(m) && choose(m*m) && choose(m*m*m)) {
+			cout << "m=" << m;
+			cout << " m*m=" << m*m;
+			cout << " m*m*m=" << m*m*m << endl;
+		}
+	return 0;
 }
 
